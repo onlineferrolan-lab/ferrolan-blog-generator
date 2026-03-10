72606e38ac69b1e42f9a0ee9523872bf06029452
onlineferrolan-lab <onlineferrolan@gmail.com>
2026-03-10 11:46:31 +0100

    Update generate-images.js

--- pages/api/generate-images.js
+++ pages/api/generate-images.js
@@ -67,7 +67,7 @@ Los prompts deben estar en inglés y ser muy específicos y descriptivos.`;
 
 async function generateImageWithGemini(prompt) {
   const apiKey = process.env.GEMINI_API_KEY;
-  const url = `https://generativelanguage.googleapis.com/v1/models/imagen-3.0-generate-001:predict?key=${apiKey}`;
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/imagen-4.0-generate-001:predict?key=${apiKey}`;
 
   const body = {
     instances: [{ prompt }],
